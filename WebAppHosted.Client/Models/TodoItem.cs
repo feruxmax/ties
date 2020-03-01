7100676920298313c6c6386cb3062fcff76905a2
feruxmax <feruxmax@gmail.com>
2020-03-01 19:16:07 +0300

    remove redundant fields

---
+++ WebAppHosted.Client/Models/TodoItem.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace WebAppHosted.Client.Models
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+    }
+}
