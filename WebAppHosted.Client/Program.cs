655897f0184b62be5530a5586f818759af96b44f
feruxmax <feruxmax@gmail.com>
2020-05-25 01:21:23 +0300

    standard oidc

--- WebAppHosted.Client/Program.cs
+++ WebAppHosted.Client/Program.cs
@@ -20,6 +20,10 @@ namespace WebAppHosted.Client
 
             builder.Services.AddBlazoredLocalStorage();
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddOidcAuthentication(options =>
+            {
+                builder.Configuration.Bind("Local", options.ProviderOptions);
+            });
 
             await builder.Build().RunAsync();
         }
